2f2c05f7ea8afa890d5f85908ee3854d4074b8cf
Isaac <23095702+iBeizsley@users.noreply.github.com>
2018-05-11 14:35:27 +0100

    Implementet HTTPWorkingDaySource & FileWorkingDaySource

---
+++ src/CR.WorkingDayService.StringSource/StringSourceExtensions.cs
@@ -0,0 +1,38 @@
+﻿// <copyright file="StringSourceExtensions.cs" company="Cognisant">
+// Copyright (c) Cognisant. All rights reserved.
+// </copyright>
+
+namespace CR.WorkingDayService.StringSource
+{
+    using System;
+
+    /// <summary>
+    /// An Extension Methods class containing methods relating to the StringWorkingDaySource.
+    /// </summary>
+    public static class StringSourceExtensions
+    {
+        /// <summary>
+        /// Configure the WorkingDayServiceBuilder to use only a String Source.
+        /// </summary>
+        /// <typeparam name="T">The type of the internal state for the String Working Day Source.</typeparam>
+        /// <param name="builder">The Builder to configure.</param>
+        /// <param name="source">The path for the file to use for the internal state of the String Working Day Source.</param>
+        /// <param name="parseFileContentAction">The action to build the internal state of the String Working Day Source from the content of the Source.</param>
+        /// <param name="checkAction">The action to check whether a given DateTime is a Working Day based on the current State of the String Working Day Source.</param>
+        /// <returns>The updated WorkingDayServiceBuilder.</returns>
+        public static WorkingDayServiceBuilder UseStringSource<T>(this WorkingDayServiceBuilder builder, string source, Func<string, T> parseFileContentAction, Func<DateTime, T, bool> checkAction)
+            => builder.UseSource(new StringWorkingDaySource<T>(source, parseFileContentAction, checkAction));
+
+        /// <summary>
+        /// Configure the WorkingDayServiceBuilder to use a String Source as well as any previously registered Sources.
+        /// </summary>
+        /// <typeparam name="T">The type of the internal state for the String Working Day Source.</typeparam>
+        /// <param name="builder">The Builder to configure.</param>
+        /// <param name="source">The path for the file to use for the internal state of the String Working Day Source.</param>
+        /// <param name="parseFileContentAction">The action to build the internal state of the String Working Day Source from the content of the Source.</param>
+        /// <param name="checkAction">The action to check whether a given DateTime is a Working Day based on the current State of the String Working Day Source.</param>
+        /// <returns>The updated WorkingDayServiceBuilder.</returns>
+        public static WorkingDayServiceBuilder AddStringSource<T>(this WorkingDayServiceBuilder builder, string source, Func<string, T> parseFileContentAction, Func<DateTime, T, bool> checkAction)
+            => builder.AddSource(new StringWorkingDaySource<T>(source, parseFileContentAction, checkAction));
+    }
+}
---
+++ src/CR.WorkingDayService.StringSource/StringWorkingDaySource.cs
@@ -0,0 +1,59 @@
+﻿// <copyright file="StringWorkingDaySource.cs" company="Cognisant">
+// Copyright (c) Cognisant. All rights reserved.
+// </copyright>
+
+namespace CR.WorkingDayService.StringSource
+{
+    using System;
+
+    /// <inheritdoc cref="IWorkingDaySource"/>
+    /// <summary>
+    /// An implementation of IWorkingDaySource which uses of a string to determine whether a given DateTime is on a Working Day or a Non-Working Day.
+    /// </summary>
+    public class StringWorkingDaySource<T> : IWorkingDaySource
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StringWorkingDaySource{T}"/> class.
+        /// </summary>
+        /// <param name="url">The url of the state from.</param>
+        /// <param name="parseAction">The action to get the state from the url content.</param>
+        /// <param name="checkAction">The action to get whether a DateTime is on a Working Day based on the current state.</param>
+        public StringWorkingDaySource(string url, Func<string, T> parseAction, Func<DateTime, T, bool> checkAction)
+            : this(parseAction, checkAction) => State = parseAction(url);
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StringWorkingDaySource{T}"/> class.
+        /// </summary>
+        /// <param name="parseAction">The action to get the state from the url content.</param>
+        /// <param name="checkAction">The action to get whether a DateTime is on a Working Day based on the current state.</param>
+        protected StringWorkingDaySource(Func<string, T> parseAction, Func<DateTime, T, bool> checkAction)
+        {
+            CheckAction = checkAction ?? throw new ArgumentNullException(nameof(parseAction));
+            ParseAction = parseAction ?? throw new ArgumentException(nameof(checkAction));
+        }
+
+        /// <summary>
+        /// Gets the action to check whether a DateTime is on a Working Day based on the internal state of the String Working Day Source.
+        /// </summary>
+        protected Func<DateTime, T, bool> CheckAction { get; }
+
+        /// <summary>
+        /// Gets the action to parse a string into the internal state of the String Working Day Source.
+        /// </summary>
+        protected Func<string, T> ParseAction { get; }
+
+        /// <summary>
+        /// Gets or sets the current internal state of the String Working Day Source.
+        /// </summary>
+        protected T State { get; set; }
+
+        /// <inheritdoc />
+        public bool IsWorkingDay(DateTime date)
+        {
+            lock (State)
+            {
+                return CheckAction(date, State);
+            }
+        }
+    }
+}
