a1029ab9a1f980cb1cf007420f6a1b63859fb272
Isaac <23095702+iBeizsley@users.noreply.github.com>
2018-06-11 14:17:23 +0100

    Implemented project

---
+++ src/EventStoreTools/EventStoreConnectionBuilder.cs
@@ -0,0 +1,69 @@
+﻿// <copyright file="EventStoreConnectionBuilder.cs" company="Cognisant">
+// Copyright (c) Cognisant. All rights reserved.
+// </copyright>
+
+namespace Cr.EventStoreTools
+{
+    using System;
+    using EventStore.ClientAPI;
+
+    /// <summary>
+    /// A helper class for quickly building an <see cref="IEventStoreConnection"/>.
+    /// </summary>
+    // ReSharper disable once UnusedMember.Global
+    public static class EventStoreConnectionBuilder
+    {
+        /// <summary>
+        /// Build an <see cref="IEventStoreConnection"/> using the passed in connection <see cref="Uri"/> and configuration.
+        /// </summary>
+        /// <param name="connection">The URI for the Event Store's external TCP port, including login details.</param>
+        /// <param name="retryLimit">An optional maximum allowed retries for any given operation; this defaults to 10.</param>
+        /// <param name="gossipTimeout">An optional gossip timeout for the EventStore connection; this defaults to 5 seconds.</param>
+        /// <param name="logger">An optional <see cref="ILogger"/> to use to log information about and issues with the <see cref="IEventStoreConnection"/>.</param>
+        /// <returns>A configured, connected <see cref="IEventStoreConnection"/> which uses any passed in configuration variables, or the defaults detailed in the parameters section.</returns>
+        // ReSharper disable once UnusedMember.Global
+        public static IEventStoreConnection SetUpEventStoreConnection(Uri connection, int retryLimit = 10, TimeSpan? gossipTimeout = null, ILogger logger = null)
+        {
+            var settings = ConnectionSettings.Create().KeepReconnecting().SetGossipTimeout(gossipTimeout ?? TimeSpan.FromSeconds(5)).LimitRetriesForOperationTo(retryLimit);
+            if (logger != null)
+            {
+                settings.UseCustomLogger(logger);
+            }
+
+            var eventStoreClient = EventStoreConnection.Create(settings, connection);
+
+            void EventStoreClientOnDisconnected(object sender, ClientConnectionEventArgs clientConnectionEventArgs) => logger?.Error($"Event Store disconnected from endpoint {clientConnectionEventArgs.RemoteEndPoint.Address}:{clientConnectionEventArgs.RemoteEndPoint.Port}");
+
+            void EventStoreClientOnConnected(object sender, ClientConnectionEventArgs clientConnectionEventArgs) => logger?.Info($"Event Store connected {clientConnectionEventArgs.RemoteEndPoint.Address}:{clientConnectionEventArgs.RemoteEndPoint.Port}");
+
+            void EventStoreClientOnReconnecting(object sender, ClientReconnectingEventArgs clientReconnectingEventArgs) => logger?.Info("Event Store reconnecting");
+
+            void EventStoreClientOnClosed(object sender, ClientClosedEventArgs clientClosedEventArgs) => logger?.Info("Event Store connection closed");
+
+            void EventStoreClientOnErrorOccurred(object sender, ClientErrorEventArgs clientErrorEventArgs)
+            {
+                if (logger == null)
+                {
+                    return;
+                }
+
+                if (clientErrorEventArgs.Exception != null)
+                {
+                    logger.Error(clientErrorEventArgs.Exception, "Event Store error occurred");
+                }
+                else
+                {
+                    logger.Error("Unknown Event Store error");
+                }
+            }
+
+            eventStoreClient.ErrorOccurred += EventStoreClientOnErrorOccurred;
+            eventStoreClient.Disconnected += EventStoreClientOnDisconnected;
+            eventStoreClient.Reconnecting += EventStoreClientOnReconnecting;
+            eventStoreClient.Connected += EventStoreClientOnConnected;
+            eventStoreClient.Closed += EventStoreClientOnClosed;
+            eventStoreClient.ConnectAsync().Wait();
+            return eventStoreClient;
+        }
+    }
+}
---
+++ src/EventStoreTools/EventStoreEventTools.cs
@@ -0,0 +1,59 @@
+﻿// <copyright file="EventStoreEventTools.cs" company="Cognisant">
+// Copyright (c) Cognisant. All rights reserved.
+// </copyright>
+
+namespace Cr.EventStoreTools
+{
+    using System;
+    using System.Text;
+    using EventStore.ClientAPI;
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// A helper class containing methods to handle reading and writing events to/from an <see cref="IEventStoreConnection"/>.
+    /// </summary>
+    public static class EventStoreEventTools
+    {
+        /// <summary>
+        /// Convert the provided event <see cref="object"/> to <see cref="EventData"/> which can be saved to an EventStore stream.
+        /// </summary>
+        /// <param name="eventId">A unique ID for the event.</param>
+        /// <param name="event">The event <see cref="object"/> to serialize and encode into <see cref="EventData"/>.</param>
+        /// <param name="serializerSettings">Optional <see cref="JsonSerializerSettings"/> to use when serializing the provided <see cref="object"/> and the event metadata.</param>
+        /// <returns>A new <see cref="EventData"/> object with the provided ID, and serialized &amp; encoded JSON data, as well as metadata containing the assembly qualified name for the event <see cref="object"/>.</returns>
+        // ReSharper disable once UnusedMember.Global
+        public static EventData ToEventData(Guid eventId, object @event, JsonSerializerSettings serializerSettings = null)
+        {
+            var data = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(@event, serializerSettings));
+            var eventHeaders = new { ClrType = @event.GetType().AssemblyQualifiedName };
+
+            var metadata = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(eventHeaders, serializerSettings));
+            var typeName = @event.GetType().Name;
+
+            return new EventData(eventId, typeName, true, data, metadata);
+        }
+
+        /// <summary>
+        /// Convert the provided <see cref="ResolvedEvent"/>'s data to the provided <see cref="Type"/>.
+        /// </summary>
+        /// <typeparam name="T">The <see cref="Type"/> to convert the <see cref="ResolvedEvent"/>'s data to.</typeparam>
+        /// <param name="event">The <see cref="ResolvedEvent"/> whose data should be converted into an <see cref="object"/> of the provided <see cref="Type"/>.</param>
+        /// <returns>An <see cref="object"/> of the provided <see cref="Type"/> build from the JSON data contained in the provided <see cref="ResolvedEvent"/>.</returns>
+        /// <param name="serializerSettings">Optional <see cref="JsonSerializerSettings"/> to use when deserializing the data in the provided <see cref="ResolvedEvent"/>.</param>
+        // ReSharper disable once UnusedMember.Global
+        public static T FromResolvedEvent<T>(ResolvedEvent @event, JsonSerializerSettings serializerSettings = null) => (T)FromResolvedEvent(@event, typeof(T), serializerSettings);
+
+        /// <summary>
+        /// Convert the provided <see cref="ResolvedEvent"/>'s data to the provided <see cref="Type"/>.
+        /// </summary>
+        /// <param name="event">The <see cref="ResolvedEvent"/> whose data should be converted into an <see cref="object"/> of the provided <see cref="Type"/>.</param>
+        /// <param name="typeToConvertTo">The <see cref="Type"/> to convert the provided <see cref="ResolvedEvent"/>'s data to.</param>
+        /// <param name="serializerSettings">Optional <see cref="JsonSerializerSettings"/> to use when deserializing the data in the provided <see cref="ResolvedEvent"/>.</param>
+        /// <returns>An <see cref="object"/> of the provided <see cref="Type"/> build from the JSON data contained in the provided <see cref="ResolvedEvent"/>.</returns>
+        public static object FromResolvedEvent(ResolvedEvent @event, Type typeToConvertTo, JsonSerializerSettings serializerSettings = null)
+        {
+            var eventString = Encoding.UTF8.GetString(@event.Event.Data);
+            return JsonConvert.DeserializeObject(eventString, typeToConvertTo, serializerSettings);
+        }
+    }
+}
