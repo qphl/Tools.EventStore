ceea4fd5fea13616cb3e8d3f60eba0947d05973f
Isaac <23095702+iBeizsley@users.noreply.github.com>
2018-06-12 17:40:22 +0100

    Fix build script project paths

--- build.cmd
+++ build.cmd
@@ -3,4 +3,4 @@
 SET VERSION=0.0.0
 IF NOT [%1]==[] (set VERSION=%1)
 
-dotnet pack src/EventStoreTools/EventStoreTools.csproj -o ../../dist -p:Version="%VERSION%" -p:PackageVersion="%VERSION%" -c Release
+dotnet pack src/Tools.EventStore/Tools.EventStore.csproj -o ../../dist -p:Version="%VERSION%" -p:PackageVersion="%VERSION%" -c Release
--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ version="0.0.0"
 if [ -n "$1" ]; then version="$1"
 fi
 
-dotnet pack src/EventStoreTools/EventStoreTools.csproj -o ../../dist -p:Version="$version" -p:PackageVersion="$version" -c Release
+dotnet pack src/Tools.EventStore/Tools.EventStore.csproj -o ../../dist -p:Version="$version" -p:PackageVersion="$version" -c Release
