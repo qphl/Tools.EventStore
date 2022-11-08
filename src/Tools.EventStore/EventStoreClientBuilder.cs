ad2368e16b90c59670435196c0f0cfe8cf406b77
James McCaig <james.mccaig@qphl.co>
2022-11-08 15:34:38 +0000

    Tools.EventStore upgrade from EventStore.Client 5.0.5 to EventStore.Client.Grpc 22.0.0 Include Newtonsoft upgrade Include .Net6.0 Upgrade to solution and project (upgraded using auto upgrade tool)

---
+++ src/Tools.EventStore/EventStoreClientBuilder.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="EventStoreClientBuilder.cs" company="Corsham Science">
+// Copyright (c) Corsham Science. All rights reserved.
+// </copyright>
+
+namespace CorshamScience.Tools.EventStore
+{
+    using System;
+    using global::EventStore.Client;
+
+    /// <summary>
+    /// A helper class for quickly building an <see cref="EventStoreClient"/>.
+    /// </summary>
+    public static class EventStoreClientBuilder
+    {
+        /// <summary>
+        /// Builds an <see cref="EventStoreClient"/> using the default settings.
+        /// </summary>
+        /// <param name="connectionString">Put valid connection string.</param>
+        /// <returns>EventStoreClient.</returns>
+        public static EventStoreClient GetEventStoreClient(string connectionString)
+        {
+            var settings = EventStoreClientSettings.Create(connectionString);
+            return new EventStoreClient(settings);
+        }
+    }
+}
